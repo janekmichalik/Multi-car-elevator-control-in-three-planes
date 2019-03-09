6360d633cf8629c6aa28ed05ddd241d8a426778d
janekmichalik <jasmichalik@gmail.com>
2019-03-09 18:24:07 +0100

    zobiektyzowanie_kodu

--- Project/Program/main.py
+++ Project/Program/main.py
@@ -7,138 +7,179 @@ import numpy as np
 import random
 from matplotlib import animation
 
-"""
-    [[2., 0., 2., 0., 2., 0., 2., 0., 2., 0., 2.],
-    [0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.],
-    [2., 0., 2., 0., 2., 0., 2., 0., 2., 0., 2.],
-    [0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.],
-    [2., 0., 2., 0., 2., 0., 2., 0., 2., 0., 2.],
-    [0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.],
-    [2., 0., 2., 0., 2., 0., 2., 0., 2., 0., 2.],
-    [0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.],
-    [2., 0., 2., 0., 2., 0., 2., 0., 2., 0., 2.],
-    [0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.],
-    [2., 0., 2., 0., 2., 0., 2., 0., 2., 0., 2.]] """
+
+class ElevatorConst:
+
+    WALL = 4
+    PATH = 0
+    ELEVATOR = 1
+    SOURCE = 3
+    DESTINATION = 2
+
+    floor_schema = """
+                    [[2., 0., 2., 0., 2., 0., 2., 0., 2., 0., 2.],
+                    [0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.],
+                    [2., 0., 2., 0., 2., 0., 2., 0., 2., 0., 2.],
+                    [0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.],
+                    [2., 0., 2., 0., 2., 0., 2., 0., 2., 0., 2.],
+                    [0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.],
+                    [2., 0., 2., 0., 2., 0., 2., 0., 2., 0., 2.],
+                    [0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.],
+                    [2., 0., 2., 0., 2., 0., 2., 0., 2., 0., 2.],
+                    [0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.],
+                    [2., 0., 2., 0., 2., 0., 2., 0., 2., 0., 2.]] """
 
 
+class Floor:
 
-# tworzymy pietra
-fig, (ax, ay) = plt.subplots(1, 2)
-fig.suptitle('Elevator', fontsize=20, fontweight='bold')
+    def __init__(self):
 
-# wymiar pietra nrowsXncols ( +1 aby konczylo sie sciana)
-nrows, ncols = 10 + 1, 10 + 1
+        self.num_of_floors_x = 1
+        self.num_of_floors_y = 2
 
-# tworzymy pietro
-floor_wall = np.zeros(nrows*ncols)
-# z macierzy 1wymiarowej robimy 2wymiarowa
-floor_wall = floor_wall.reshape((nrows, ncols))
+        self.floor_rows = 10 + 1
+        self.floor_cols = self.floor_rows
 
-# budujemy ulozenie pietra (sciezki/sciany)
-# 4 - sciana
-# 0 - sciezka
-# 3 - source
-# 2 - destination
-for wiersz in range(nrows):
-    for kolumna in range(ncols):
-        if wiersz % 2 == 0 and kolumna % 2 == 0:
-            floor_wall[wiersz][kolumna] = 4
+        self.floor = None
+        self.create_floor()
 
-# generowanie punktu startowego
-source_x = 0
-source_y = 0
-SOURCE = []
+    def create_floor(self):
+        """Function which create floor schema:
+        # 4 - wall
+        # 0 - path
+        # 3 - source
+        # 2 - destination
+        """
 
-while floor_wall[source_x][source_y] == 4:
-    source_x = random.randint(0, 10)
-    source_y = random.randint(0, 10)
-SOURCE = [source_x, source_y]
-floor_wall[source_x][source_y] = 3
+        self.floor = np.zeros(self.floor_rows * self.floor_cols)
+        self.floor = self.floor.reshape((self.floor_rows, self.floor_cols))
 
-# generowanie punktu koncowego
-destination_x = 0
-destination_y = 0
-DESTINATION = []
-
-while floor_wall[destination_x][destination_y] == 4 and (source_x != destination_x or source_y != destination_y):
-    destination_x = random.randint(0, 10)
-    destination_y = random.randint(0, 10)
-DESTINATION = [destination_x, destination_y]
-floor_wall[destination_x][destination_y] = 2
+        for row in range(self.floor_rows):
+            for col in range(self.floor_cols):
+                if row % 2 == 0 and col % 2 == 0:
+                    self.floor[row][col] = ElevatorConst.WALL
 
 
-def najkrotsza_sciezka(floor_wall, start):
-    queue = collections.deque([[start]])
-    seen = []
-    seen.append(set(start))
-    while queue:
-        path = queue.popleft()
-        x, y = path[-1]
-        if floor_wall[x][y] == 2:
-            return path
-        for x2, y2 in ((x+1, y), (x-1, y), (x, y+1), (x, y-1)):
-            if 0 <= x2 < nrows and 0 <= y2 < ncols and floor_wall[y2][x2] != 4 and (x2, y2) not in seen:
-                queue.append(path + [(x2, y2)])
-                seen.append((x2, y2))
+class Elevator(Floor):
+
+    def __init__(self):
+
+        super().__init__()
+
+        self.source_x = 0
+        self.source_y = 0
+        self.source = [self.source_x, self.source_y]
+
+        self.shortest_path = None
+
+        self.dest_x = 0
+        self.dest_y = 0
+        self.destination = [self.dest_x, self.dest_y]
+        self.generate_starting_point()
+        self.generate_ending_point()
+        self.compute_shortest_path()
+
+    def generate_starting_point(self):
+
+        while self.floor[self.source_x][self.source_y] == 4:
+            self.source_x = random.randint(0, 10)
+            self.source_y = random.randint(0, 10)
+        self.source = [self.source_x, self.source_y]
+        self.floor[self.source_x, self.source_y] = ElevatorConst.SOURCE
+
+    def generate_ending_point(self):
+
+        while self.floor[self.dest_x][self.dest_y] == 4 \
+                and (self.source_x != self.dest_x or self.source_y != self.dest_y):
+            self.dest_x = random.randint(0, 10)
+            self.dest_y = random.randint(0, 10)
+        self.destination = [self.dest_x, self.dest_y]
+        self.floor[self.dest_x][self.dest_y] = ElevatorConst.DESTINATION
+
+    def compute_shortest_path(self):
+        queue = collections.deque([[self.source]])
+        seen = []
+        seen.append(set(self.source))
+        while queue:
+            self.shortest_path = queue.popleft()
+            x, y = self.shortest_path[-1]
+            if self.floor[x][y] == 2:
+                return
+            for x2, y2 in ((x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)):
+                if 0 <= x2 < self.floor_rows and 0 <= y2 < self.floor_cols and self.floor[y2][x2] != 4 and (x2, y2) not in seen:
+                    queue.append(self.shortest_path + [(x2, y2)])
+                    seen.append((x2, y2))
 
 
-def aktualizowanie_danych(data):
-    floor_1.set_data(data)
-    floor_2.set_data(data)
-    return floor_1, floor_2
+class Plot(Elevator):
+
+    def __init__(self):
+
+        super().__init__()
+        self.fig = None
+        self.ax, self.ay = None, None
+        self.row_labels = range(self.floor_rows)
+        self.col_labels = self.row_labels
+
+        self.fig, (self.ax, self.ay) = plt.subplots(self.num_of_floors_x, self.num_of_floors_y)
+        self.floor_1 = self.ax.imshow(self.floor, cmap=plt.cm.get_cmap('Accent'))
+        self.floor_2 = self.ay.imshow(self.floor, cmap=plt.cm.get_cmap('Accent'))
+        self.ani = animation.FuncAnimation(self.fig, self.update, self.data, interval=500, save_count=50)
+        self.values = np.arange(0, 5, 1)
+        self.labels = ["Path", "Elevator", "Destination", "Source", "Wall"]
+        self.colors = [self.floor_1.cmap(self.floor_1.norm(value)) for value in self.values]
+        self.patches = [mpatches.Patch(color=self.colors[i], label=f"{self.labels[i]}") for i in range(len(self.values))]
+
+        self.draw_plot()
+
+    def update(self, data):
+        self.floor_1.set_data(data)
+        self.floor_2.set_data(data)
+        return self.floor_1, self.floor_2
+
+    def data(self):
+        for row in range(self.floor_rows):
+            for col in range(self.floor_cols):
+                if row == self.dest_x and col == self.dest_y:
+                    self.ani.event_source.stop()
+                if self.floor[row][col] == ElevatorConst.PATH:
+                    self.floor[row][col] = ElevatorConst.ELEVATOR
+                    yield self.floor
+                if self.floor[row][col] == ElevatorConst.ELEVATOR:
+                    self.floor[row][col] = ElevatorConst.PATH
+                    yield self.floor
+
+    def logs(self):
+
+        pprint(self.floor)
+
+        print(f"Destination: ({self.destination})")
+        print(f"Source: ({self.source})")
+
+        path = self.shortest_path
+        print(path)
+        if list(path[-1]) == self.destination:
+            print("Destination succeeded")
+        if path[0] == self.source:
+            print("Source succeeded")
+
+    def draw_plot(self):
+
+        self.ax.set_title('Floor no 1')
+        self.ay.set_title('Floor no 2')
+
+        self.ax.set_xticks(self.row_labels)
+        self.ax.set_yticks(self.col_labels)
+
+        self.ay.set_xticks(self.row_labels)
+        self.ay.set_yticks(self.col_labels)
+
+        plt.legend(handles=self.patches, bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+        plt.show()
 
 
-def dane_do_animacji():
-    for wiersz in range(nrows):
-        for kolumna in range(ncols):
-            if wiersz == DESTINATION[0] and kolumna == DESTINATION[1]:
-                ani.event_source.stop()
-            if floor_wall[wiersz][kolumna] == 0:
-                floor_wall[wiersz][kolumna] = 1
-                yield floor_wall
-            if floor_wall[wiersz][kolumna] == 1:
-                floor_wall[wiersz][kolumna] = 0
-                yield floor_wall
+def main():
+    simulation = Plot()
+    simulation.logs()
 
-
-
-row_labels = range(nrows)
-col_labels = row_labels
-
-ax.set_title('Floor no 1')
-ay.set_title('Floor no 2')
-
-ax.set_xticks(row_labels)
-ax.set_yticks(col_labels)
-
-ay.set_xticks(row_labels)
-ay.set_yticks(col_labels)
-
-pprint(floor_wall)
-
-floor_1 = ax.imshow(floor_wall, cmap=plt.cm.get_cmap('Accent'))
-floor_2 = ay.imshow(floor_wall, cmap=plt.cm.get_cmap('Accent'))
-
-
-values = np.arange(0, 5, 1)
-labels = ["Sciezka", "Winda", "Cel", "Poczatek", "Sciana"]
-
-colors = [floor_1.cmap(floor_1.norm(value)) for value in values]
-patches = [mpatches.Patch(color=colors[i], label=f"{labels[i]}") for i in range(len(values))]
-
-
-plt.legend(handles=patches, bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
-
-print(f"Destination: ({DESTINATION[0]}, {DESTINATION[1]})")
-print(f"Source: ({SOURCE[0]}, {SOURCE[1]})")
-path = najkrotsza_sciezka(floor_wall, SOURCE)
-print(path)
-if list(path[-1]) == DESTINATION:
-    print("Destination succeeded")
-if path[0] == SOURCE:
-    print("Source succeeded")
-
-ani = animation.FuncAnimation(fig, aktualizowanie_danych, dane_do_animacji, interval=500,
-                              save_count=50, blit=True)
-
-plt.show()
+main()
